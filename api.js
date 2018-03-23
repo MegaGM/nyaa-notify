8d762a1d0b233516dd2270404ba134291a337f0e
Mega <Aza.Noriega@gmail.com>
2018-03-24 03:55:11 +0500

    add simple api

--- api.js
+++ api.js
@@ -1,21 +1,27 @@
+let store = new (require('conf'))({
+  cwd: process.cwd(),
+  configName: 'database',
+})
+console.info(store)
 const
   Promise = require('bluebird'),
   fs = require('fs-extra'),
   axios = require('axios'),
   fastXmlParser = require('fast-xml-parser'),
-  version = require('./package.json').version,
-  headers = {
-    'User-Agent': `NyaaNotify v${version}`,
-  }
+  version = require('./package.json').version
+// headers = {
+//   'User-Agent': `NyaaNotify v${version}`,
+// }
 
 console.info(`NyaaNotify v${version}`)
 
 export function fetchQuery(q) {
-  let uri = 'https://nyaa.si/?f=0&c=1_2&q=' + q
-  return axios.get(uri, { headers })
+  let uri = 'https://nyaa.si/?f=0&c=1_2&page=rss&q=' + q
+  return axios.get(uri)
     .then(r => fastXmlParser.parse(r.data))
     .then(json => {
-      console.info('what is data: ', json)
-      return json
+      let items = json.rss.channel.item
+      console.info(items)
+      return items
     })
 }
