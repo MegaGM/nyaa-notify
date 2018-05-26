e9ed39be02fffbbc016841463353e1834d33c77f
Mega <Aza.Noriega@gmail.com>
2018-05-26 14:25:17 +0500

    add utilities to mess with nyaa-manager-database.json

---
+++ dbMessing.js
@@ -0,0 +1,70 @@
+'use strict'
+const Promise = require('bluebird')
+const qualityRegex = /(.+) (480p|720p|1080p|1440p)/
+
+const
+  path = require('path'),
+  { exec } = require('child_process'),
+  store = require('./src/makeStore')()
+
+console.info('store.path: ', store.path)
+
+class DBMixer {
+  constructor() {
+    this.db = store.get('db')
+  }
+
+  dbSaveToHDD() {
+    store.set('db', this.db)
+  }
+
+  mess() {
+    let
+      animeHash = {},
+      anime = this.db.anime
+
+    anime.map(animeQ => {
+      // console.info(animeQ.q)
+      animeQ.q = animeQ.q.replace(qualityRegex, (input, q, quality) => {
+        q = q.trim()
+        quality = quality.trim()
+        // console.info(quality, q)
+        // console.info(q)
+        // console.info(animeHash[q])
+
+        if (!animeHash[q]) {
+          animeHash[q] = {
+            q: q,
+            quality: [quality],
+            items: [],
+          }
+        } else {
+          if (!animeHash[q].quality.includes(quality))
+            animeHash[q].quality.push(quality)
+        }
+
+        return q
+      })
+    })
+    // console.info(animeHash)
+
+    anime.map(animeQ => {
+      if (!animeHash[animeQ.q]) {
+        animeHash[animeQ.q] = animeQ
+        return
+      }
+
+      animeQ.items.map(a => animeHash[animeQ.q].items.push(a))
+    })
+
+    this.db.anime = []
+    for (let q in animeHash)
+      this.db.anime.push(animeHash[q])
+
+    console.info('DBBB: ', this.db)
+    this.dbSaveToHDD()
+  }
+}
+
+const db = new DBMixer()
+db.mess()
